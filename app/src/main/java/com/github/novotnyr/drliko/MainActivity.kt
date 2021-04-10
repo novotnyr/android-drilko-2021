5bfbd66fe947fd53454b054722ae69b346f40222
Robert Novotny <novotnr0@gmail.com>
2021-04-11 00:03:41 +0200

    Pridana zamena fragmentov po kliknuti na mesto

--- app/src/main/java/com/github/novotnyr/drliko/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/drliko/MainActivity.kt
@@ -2,15 +2,25 @@ package com.github.novotnyr.drliko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+    private val cityViewModel: CityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.commit {
             replace(R.id.dynamicFragmentMainActivity, MasterFragment())
+        }
+
+        cityViewModel.selectedCity.observe(this) {
+            supportFragmentManager.commit {
+                replace(R.id.dynamicFragmentMainActivity, FragmentDetail())
+                addToBackStack(null)
+            }
         }
     }
 }
