a595a35bdbf610da85d36cf18e918d505925f410
Robert Novotny <novotnr0@gmail.com>
2021-04-10 23:15:15 +0200

    Pridany kod detailoveho fragmentu

--- app/src/main/java/com/github/novotnyr/drliko/FragmentDetail.kt
+++ app/src/main/java/com/github/novotnyr/drliko/FragmentDetail.kt
@@ -1,7 +1,21 @@
 package com.github.novotnyr.drliko
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.novotnyr.drliko.databinding.FragmentDetailBinding
 
 class FragmentDetail : Fragment() {
+    lateinit var binding: FragmentDetailBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
