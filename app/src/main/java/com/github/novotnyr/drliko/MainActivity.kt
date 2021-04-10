f49c7e1fcea30aea09e41624b225bac774b578aa
Robert Novotny <novotnr0@gmail.com>
2021-04-10 23:09:50 +0200

    Pridane dynamicke nahradzanie fragmentu

--- app/src/main/java/com/github/novotnyr/drliko/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/drliko/MainActivity.kt
@@ -2,10 +2,15 @@ package com.github.novotnyr.drliko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace(R.id.dynamicFragmentMainActivity, MasterFragment())
+        }
     }
 }
