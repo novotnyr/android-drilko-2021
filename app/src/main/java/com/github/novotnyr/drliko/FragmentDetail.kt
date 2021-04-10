75943eb64a265af2e4586409efa21a31abb086e2
Robert Novotny <novotnr0@gmail.com>
2021-04-10 23:54:09 +0200

    Prepojeny viewmodel a detailovy fragment, ktory sleduje zmeny vyberu mesta

--- app/src/main/java/com/github/novotnyr/drliko/FragmentDetail.kt
+++ app/src/main/java/com/github/novotnyr/drliko/FragmentDetail.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.github.novotnyr.drliko.databinding.FragmentDetailBinding
 
 class FragmentDetail : Fragment() {
     lateinit var binding: FragmentDetailBinding
+
+    private val cityViewModel: CityViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +20,14 @@ class FragmentDetail : Fragment() {
     ): View {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        cityViewModel.selectedCity.observe(viewLifecycleOwner, ::setTemperature)
+    }
+
+    private fun setTemperature(city: String) {
+        val temperature = city.hashCode() % 32 // vypocet teploty pre mesto
+        binding.temperatureTextView.text = "$temperature °C"
     }
 }
